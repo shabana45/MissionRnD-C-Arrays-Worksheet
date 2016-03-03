0dfb0896231ab625cbdac93fe2ed632b598f71bf
shabana45 <shabana45@missionrnd.com>
2016-03-03 09:48:40 +0530

    Submit Commit

--- src/scoresDescendingSort.cpp
+++ src/scoresDescendingSort.cpp
@@ -21,5 +21,22 @@ struct student {
 };
 
 void * scoresDescendingSort(struct student *students, int len) {
-	return NULL;
+	int i, j;
+	if (students == '\0' || len <= 0)
+		return NULL;
+	else {
+		for (i = 0; i < len; i++)
+		{
+			for (j = 0; j < len - i; j++)
+			{
+				if (students[j].score < students[j + 1].score)
+				{
+					students[j].score = students[j].score^students[j + 1].score;
+					students[j + 1].score = students[j].score^students[j + 1].score;
+					students[j].score = students[j].score^students[j + 1].score;
+				}
+			}
+		}
+		return NULL;
+	}
 }
